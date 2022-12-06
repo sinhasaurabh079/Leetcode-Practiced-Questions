b483fa77ba6950ea40c55896f1eca9fa54e63d36
Saurabh Sinha <sinhasaurabh079@gmail.com>
2022-12-06 23:10:54 +0530

    Update 231.Power-of-Two.cpp

--- Maths/231.Power-of-Two.cpp
+++ Maths/231.Power-of-Two.cpp
@@ -21,9 +21,11 @@ public:
         if(n == 0)
           return false;
         while(n%2==0)
-          n = n/2;
-       // as 2^x : 1,2,4,8,.... 
-          return (n==1);    
+              n = n/2;
+              // as 2^x : 1,2,4,8,.... 
+              return (n==1); 
+        return false;
+
     }
 };
 
