7ce787dce76c4ec08a4c20e1eaca989330687775
Saurabh Sinha <sinhasaurabh079@gmail.com>
2022-12-15 22:54:54 +0530

    Update 557-reverse-words-in-a-string-iii.cpp

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,3 +1,4 @@
+//using reverse function
 class Solution {
 public:
     string reverseWords(string s) {
@@ -24,5 +25,21 @@ till now->  ="s'teL ekat edoCteeL contest""
         // to reverse the last word
         reverse(s.begin() + i, s.end());
         return s;
+    }
+};
+
+// using stringstream
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+        string t, ans;
+        
+        while (getline(ss, t, ' ')) {
+            reverse(t.begin(), t.end());
+            ans += t + ' ';
+        }
+        // We will take substring of one less size as there will be space after last word which is not required.
+        return ans.substr(0, ans.size() - 1); 
     }
 };
