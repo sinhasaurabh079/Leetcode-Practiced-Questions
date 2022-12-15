246f270fb9086fc76cff5799fc68131b5cb4b222
Saurabh Sinha <sinhasaurabh079@gmail.com>
2022-12-15 22:52:51 +0530

    Time: 14 ms (90.43%), Space: 9.5 MB (99.12%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        stringstream ss(s);
-        string t, ans;
-        
-        while (getline(ss, t, ' ')) {
-            reverse(t.begin(), t.end());
-            ans += t + ' ';
+        int i=0;
+        for(int j=0;j<s.length();j++)
+        {
+            if(s[j]==' ')
+            {  // ex - hackers so
+               // space dikhega uske baad hum hackers ko reverse krdenge
+               // as s.begin()+i gives points first character and s.begin()+j points last character of word before space 
+                reverse(s.begin()+i,s.begin()+j);
+                i = j+1;    // setting the i pointer to point the first character of another word
+            }
         }
-        // We will take substring of one less size as there will be space after last word which is not required.
-        return ans.substr(0, ans.size() - 1); 
+        /*
+  Input-> s ="Let's take LeetCode contest"
+  Output->  ="s'teL ekat edoCteeL contest"
+ Expected-> ="s'teL ekat edoCteeL tsetnoc" 
+till now->  ="s'teL ekat edoCteeL contest""
+                                  ^^^^^^^ 
+              the contest has not reversed as
+              we '\0' encountered while loop terminates and i is pointing to the first character of the last word
+        */
+        // to reverse the last word
+        reverse(s.begin() + i, s.end());
+        return s;
     }
 };
