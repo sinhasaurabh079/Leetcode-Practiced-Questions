4b690e3d508a55e4a56904da322b9cd144bd6fa8
Saurabh Sinha <sinhasaurabh079@gmail.com>
2022-12-21 23:09:53 +0530

    Update 389-find-the-difference.cpp

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -11,3 +11,29 @@ public:
         return (char)sum;  //explicitly converting ascii value to character 
     }
 };
+
+// for large sum
+    char findTheDifference(string s, string t) {
+        int sum = 0;
+        for (auto &c: t)
+        {
+            sum = sum + c;
+            sum = sum % 256;
+        }
+        for (auto &c: s)
+        {
+             sum = sum - c;
+            sum = sum % 256;
+        }
+        return (char)sum;
+    }
+
+// using sorting
+    char findTheDifference(string s, string t) {
+        int n = s.length();
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        for(int i=0; i<n; i++) if(s[i]!=t[i]) return t[i];
+        return t[n];
+    }
+//
