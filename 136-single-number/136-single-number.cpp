f587b1179be09d7a3879de8763f9b9c62dbf2559
Saurabh Sinha <sinhasaurabh079@gmail.com>
2022-12-21 23:36:47 +0530

    Update 136-single-number.cpp

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,3 +1,4 @@
+// using map
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
@@ -13,3 +14,21 @@ public:
     }
 };
 
+
+// using xor operation
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+
+        /*
+    A^A=0
+    A^B^A=B
+    (A^A^B) = (B^A^A) = (A^B^A) = B This shows that position doesn't matter.
+    Similarly , if we see , a^a^a......... (even times)=0 and a^a^a........(odd times)=a
+       */
+        int ans = 0;
+        for(auto x:nums)
+           ans = ans^x;
+        return ans;   
+    }
+};
