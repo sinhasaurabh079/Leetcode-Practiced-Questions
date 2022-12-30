a016dbbabf84358a1130ae7c898f9d2c1877fe7e
Saurabh Sinha <sinhasaurabh079@gmail.com>
2022-12-30 14:25:33 +0530

    Update 0771-jewels-and-stones.cpp

--- 0771-jewels-and-stones/0771-jewels-and-stones.cpp
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -14,3 +14,38 @@ public:
         // T.C. = O(j+s)  instead of O(js)
     }
 };
+
+// using for loop
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        if(jewels.length() > stones.length())
+            swap(jewels,stones);
+        
+        int cnt=0;
+        for(int i=0;i<jewels.length();i++)
+        {
+            for(int j=0;j<stones.length();j++)
+            {
+                if(jewels[i] == stones[j])
+                    cnt++;
+            }
+        }
+        return cnt;
+        
+    }
+};
+
+// using set
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int cnt=0;
+        unordered_set<char> setj(jewels.begin(),jewels.end());
+        
+        for(char c: stones)
+          if(setj.count(c))   // agar wo character set m present ho tb
+            cnt++;
+        return cnt;
+    }
+};
