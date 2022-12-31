61b2b872fe76e47b37c60de345b0b12c46e6d40d
Saurabh Sinha <sinhasaurabh079@gmail.com>
2022-12-31 18:11:34 +0530

    Update 1470-shuffle-the-array.cpp

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -11,3 +11,20 @@ public:
         return nums;
     }
 };
+
+// using map
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        map<int,char> mp;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            mp[indices[i]]=s[i];
+        }
+        string ans="";
+        for(auto value:mp){
+            ans.push_back(value.second);
+        }
+        return ans;
+    }
+};
