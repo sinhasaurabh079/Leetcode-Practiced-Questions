cd63c65f4705e97889f44e2a6746abc0acbfee75
Saurabh Sinha <sinhasaurabh079@gmail.com>
2023-01-13 20:17:48 +0530

    Update 31-next-permutation.cpp

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,30 +1,69 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int curr = -1;
-        int i = nums.size() - 1;
-        while (i > 0) {
-            if (nums[i - 1] < nums[i]) {
-                curr = i - 1;
+
+      // Algorithm :
+      // 1. Find index of element i.e. to be swapped from right to left
+      // 2. Then find the just maximum value to this element, which should be lesser than element at swapping+1 
+      // 3. Perform swap Operation
+      // 4. Reverese the array from swapping index + 1 to last
+        int swapping_index=-1;
+        
+        for(int i=nums.size()-1;i>0;i--){
+            if(nums[i] > nums[i-1]){
+                swapping_index=i-1;
                 break;
             }
-            i--;
-        }
-        if (curr == -1) {
-            sort(nums.begin(), nums.end());
-        } else {
-            int m = curr + 1;
-            while (i < nums.size()) {
-                if (nums[i] > nums[curr] && nums[i] < nums[m]) {
-                    m = i;
-                }
-                i++;
+        }    
+
+            if(swapping_index==-1){
+                sort(nums.begin(),nums.end());
             }
-            int temp = nums[m];
-            nums[m] = nums[curr];
-            nums[curr] = temp;
-            sort(nums.begin() + curr + 1, nums.end());
-        }
+            else{
+                int max_index = swapping_index + 1;
+
+                for(int i=swapping_index+2;i<nums.size();i++){
+                    if(nums[i] > nums[swapping_index] && nums[i] < nums[max_index])
+                         max_index=i;
+                }
+                swap(nums[swapping_index],nums[max_index]);
+            
+                int k=swapping_index+1;
+                sort(nums.begin()+k,nums.end());
+            }          
     }
 };
 
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+
+        int curr=-1;
+        
+        for(int i=nums.size()-1;i>0;i--)
+        {
+            if(nums[i] > nums[i-1])
+            {
+                curr=i-1;
+                break;
+            }
+        }    
+
+            if(curr==-1)
+            {
+                sort(nums.begin(),nums.end());
+            }
+            else
+            {
+                int m = curr + 1;
+
+                for(int i=curr+2;i<nums.size();i++){
+
+                    if(nums[i] > nums[curr] && nums[i] < nums[m])
+                         m=i;
+                }
+                swap(nums[curr],nums[m]);
+                sort(nums.begin()+curr+1,nums.end());
+            }           
+    }
+};
