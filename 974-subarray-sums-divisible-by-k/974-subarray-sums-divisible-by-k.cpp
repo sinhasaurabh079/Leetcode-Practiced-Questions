5686b82c455bd1271a4a3f9323e972b6845baa30
Saurabh Sinha <sinhasaurabh079@gmail.com>
2023-01-19 22:42:36 +0530

    Time: 53 ms (85.71%), Space: 30.2 MB (86.10%) - LeetHub

---
+++ 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        vector<int> output(k, 0);
+        // prefixModSum is used to store sum of all prefix mods
+        int prefixModSum = 0;
+        int res = 0;
+        output[0] = 1;
+        for(int i = 0; i < nums.size(); i++){
+            // (nums[i] % k + k) is used to avoid nagative remsinder
+            prefixModSum = (prefixModSum + nums[i] % k + k) % k;
+            //If in test case, only positive number given like [5,0,5,3] then
+            //prefixModSum = prefixModSum + nums[i] % k;
+            // Add the count of subarrays that have the same remainder
+            res = res + output[prefixModSum];
+            output[prefixModSum]++;
+        }
+        return res;
+    }
+};
+/*
+ye kaam nhi kiya 
+
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int sum = 0;
+        int count=0;
+
+        for(int i=0;i<nums.size();i++){
+            sum=0;
+            for(int j=i;j<nums.size();j++){
+                sum += nums[j];
+
+                if(sum%k==0)
+                   count++;
+            }
+        }
+        return count;
+    }
+};
+*/
