f22364d224bb7f289717b5a5c2d412f902acc8ae
Saurabh Sinha <sinhasaurabh079@gmail.com>
2023-01-19 23:03:02 +0530

    Time: 26 ms (99.62%), Space: 28.2 MB (57.31%) - LeetHub

---
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n=nums.size();
+        int i,j;
+        int cur_size=0,min_size=n,sum=0;
+        bool flag = false;
+
+        for(i=0,j=0;sum >= target || j<n; ){
+            // expansion to reach target
+            if(sum < target){
+                sum += nums[j++];
+                cur_size++;
+            }
+            // shrinkng when sum becomes greater than or equal to target
+            // then we try to find min size window with sum >= target
+            else{
+                if(min_size > cur_size)
+                     min_size = cur_size;
+
+                flag = true;   
+                // picha se hataya   
+                sum -= nums[i++];
+                // kuki current window ki size shrink huyi h
+                cur_size--;   
+            }
+        }
+        if(flag)
+           return min_size;
+        else
+           return 0;     
+    } 
+};
+
+/*
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n=nums.size();
+        int i,j;
+        int cur_size=0,min_size=n,sum=0;
+        
+        for(i=0;i<n;i++)
+          sum += nums[i];
+
+        if(sum < target)  
+          return 0;
+
+        sum = 0;
+        for(i=0,j=0;sum >= target || j<n; ){
+            // expansion
+            if(sum < target){
+                sum += nums[j++];
+                cur_size++;
+            }
+            // shrinkng when sum becomes greater than or equal to target
+            // then we try to find min size window with sum >= target
+            else{
+                if(min_size > cur_size)
+                   min_size = cur_size;
+                // picha se hataya   
+                sum -= nums[i++];
+                // kuki current window ki size shrink huyi h
+                cur_size--;   
+            }
+        }
+        return min_size;  
+    }
+};
+*/
