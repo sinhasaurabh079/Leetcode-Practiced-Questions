cbe2fe4b7e6dc6fc24d70d6b30fc640df13e9c92
Saurabh Sinha <sinhasaurabh079@gmail.com>
2023-01-20 22:33:23 +0530

    Time: 88 ms (59.28%), Space: 22.2 MB (76.03%) - LeetHub

---
+++ 491-non-decreasing-subsequences/491-non-decreasing-subsequences.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void solve(vector<int>&nums,int index,vector<int>&output,set<vector<int>>&ans)
+    {
+        if(index >= nums.size()){
+            if(output.size()>1)
+              ans.insert(output);
+           return;   
+        }
+
+        if(output.size()==0 || nums[index] >= output.back()){
+            // If output is empty or current element is 
+            // greater than or equal to the last element in output
+            output.push_back(nums[index]);
+            solve(nums,index+1,output,ans);
+            output.pop_back();
+        }
+
+        solve(nums,index+1,output,ans);
+    }
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        // output for array and set is used to remove duplicates vector
+        vector<int> output;
+        set<vector<int>> ans;
+
+        solve(nums,0,output,ans);
+
+        return vector(ans.begin(),ans.end());
+    }
+};
