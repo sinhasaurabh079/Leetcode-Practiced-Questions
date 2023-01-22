f3462b10f3e240c41bc675a6445d8a8c54d35fa9
Saurabh Sinha <sinhasaurabh079@gmail.com>
2023-01-22 19:40:16 +0530

    Time: 3 ms (23.08%), Space: 6 MB (69.23%) - LeetHub

---
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    Solution(){ 
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    }
+    int alternateDigitSum(int n) {
+        vector<long long> ans;
+        int digit=0,x=0;
+        long long sum=0,count=0;
+      
+        x=n;
+        while(n)
+        {
+            digit = n%10;
+            //ans.push_back(digit);
+            count++;
+            n=n/10;
+            
+        }
+        count--;
+        while(x && count>=0)
+        {
+            digit = x%10;
+            if(count%2==0)
+                 sum += digit;
+            else
+                 sum -= digit;
+            x/=10;
+            count--;
+        }    
+            
+         // memoery limit exceeded for this   
+        // for(int i=0;i<ans.size();i++)
+        // {
+        //     if(i%2==0)
+        //         sum += ans[i];
+        //     else
+        //         sum -= ans[i];
+        // }
+        return sum;
+    }
+};
