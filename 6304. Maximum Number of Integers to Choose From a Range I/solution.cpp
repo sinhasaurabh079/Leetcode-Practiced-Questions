3c2ac401d74b590785e6a6d312f59bc276024a93
Saurabh Sinha <sinhasaurabh079@gmail.com>
2023-02-04 22:16:35 +0530

    Update solution.cpp

--- 6304. Maximum Number of Integers to Choose From a Range I/solution.cpp
+++ 6304. Maximum Number of Integers to Choose From a Range I/solution.cpp
@@ -19,3 +19,21 @@ public:
         return count;
     }
 };
+// t.c = O(nlogn)
+
+// using map
+class Solution {
+public:
+    int maxCount(vector<int>& banned, int n, int maxSum) {
+        map <int, int> m;
+        for(int i : banned)
+            m[i] = 1;
+        int ans = 0, sum = 0;
+        for(int i = 1; i <= n; ++i){
+            if(sum + i <= maxSum && m[i] == 0)
+                sum += i, ans++;
+        }
+        return ans;
+    }
+};
+// t.c = O(n)
