a70656ca132bd22983fd2aa66a2b9446d2b18974
Saurabh Sinha <sinhasaurabh079@gmail.com>
2023-02-15 20:13:51 +0530

    Update 989-add-to-array-form-of-integer.cpp

--- 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.cpp
+++ 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     vector<int> addToArrayForm(vector<int>& num, int K) {
+        /*
+       Algo
+        * piche se addition krengey
+        * sum ka unit place wale ko current position pr dalengey
+          and bache hue part of sum ko carry m bhejengey
+        * if array get exhausted while the carry not
+          add that carry at the front   
+      */
          for (int i = num.size() - 1; i >= 0 && K > 0; --i) {
             num[i] += K;
             K = num[i] / 10;
